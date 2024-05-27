d441a4f85f97aa100ea95db0a64172701fcd60c2
FerozShaik96 <ferozshaik1997@gmail.com>
2024-05-27 12:16:59 +0530

    Font optimization and Laoding them and added little bit styling and Using Image tag  from Next js Optimising the image

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,11 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
+import { Josefin_Sans } from "next/font/google";
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+console.log(josefin);
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 export const metadata = {
   title: {
     template: "%s / The Wild Oasis",
@@ -13,12 +18,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-primary-950 text-gray-50">
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
+      <body
+        className={`${josefin.className} flex min-h-screen flex-col bg-primary-950 text-gray-50 antialiased`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="mx-auto max-w-7xl">{children}</main>
+        </div>
       </body>
     </html>
   );
