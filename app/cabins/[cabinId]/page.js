5b7da64a62b4ee48f81efbb2cb158050d427c0ad
FerozShaik96 <ferozshaik1997@gmail.com>
2024-06-01 16:32:17 +0530

    Generating Dtnamic Metadata

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,6 +1,10 @@
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import { getCabin } from "@/app/_lib/data-service";
+export async function generateMetadata({ params }) {
+  const { name } = await getCabin(params.cabinId);
+  return { title: `Cabin ${name}` };
+}
 
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinId);
