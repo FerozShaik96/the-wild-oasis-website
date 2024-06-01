6e2c8edb317701383f4bade1b149b049b59a4b2e
FerozShaik96 <ferozshaik1997@gmail.com>
2024-06-01 17:42:16 +0530

    for dynamic roues added a local not found page

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,5 +1,6 @@
 import { eachDayOfInterval } from "date-fns";
 import { supabase } from "./supabase";
+import { notFound } from "next/navigation";
 export const getCabins = async function () {
   const { data, error } = await supabase
     .from("cabins")
@@ -25,6 +26,7 @@ export async function getCabin(id) {
 
   if (error) {
     console.error(error);
+    notFound();
   }
 
   return data;
