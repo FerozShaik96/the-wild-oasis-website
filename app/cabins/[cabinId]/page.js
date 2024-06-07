c3d564332bc04c3600f69ce86c45926626a109d9
FerozShaik96 <ferozshaik1997@gmail.com>
2024-06-07 14:47:42 +0530

    Added a text Expender In Dynamic Cabin Page

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,14 +1,15 @@
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import { getCabin, getCabins } from "@/app/_lib/data-service";
+import TextExpander from "@/app/_components/TextExpander";
 export async function generateMetadata({ params }) {
   const { name } = await getCabin(params.cabinId);
   return { title: `Cabin ${name}` };
 }
-export async function generateStaticParams(){
+export async function generateStaticParams() {
   const cabins = await getCabins();
-  const ids = cabins.map((cabin)=>({cabinId:String(cabin.id)}))
-  return ids
+  const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
+  return ids;
 }
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinId);
@@ -32,7 +33,9 @@ export default async function Page({ params }) {
             Cabin {name}
           </h3>
 
-          <p className="mb-10 text-lg text-primary-300">{description}</p>
+          <p className="mb-10 text-lg text-primary-300">
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className="mb-7 flex flex-col gap-4">
             <li className="flex items-center gap-3">
