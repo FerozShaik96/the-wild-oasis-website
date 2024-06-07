7c5c2c87858beffa085c644a81131b030095b25f
FerozShaik96 <ferozshaik1997@gmail.com>
2024-06-07 23:49:40 +0530

    Reading the Search Params USing searchparams

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,12 +1,14 @@
 import CabinList from "@/app/_components/CabinList";
 import { Suspense } from "react";
 import Spinner from "@/app/_components/Spinner";
+import Filter from "@/app/_components/Filter";
 
 export const revalidate = 3600;
 export const metadata = {
   title: "Cabins",
 };
-export default function Page() {
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
   return (
     <div>
       <h1 className="mb-5 text-4xl font-medium text-accent-400">
@@ -20,8 +22,11 @@ export default function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="mb-8 flex justify-end">
+        <Filter />
+      </div>
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
